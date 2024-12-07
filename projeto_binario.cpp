d360ed767d17b38f90e1c053c2b2b7324a965c03
Gabriel Lineker <gabriel.lineker@estudante.ufjf.br>
2024-12-07 20:38:52 -0300

    Update projeto_binario.cpp

---
+++ projeto_binario.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void converte_em_binario(int num){
+    int quociente = num, resto;
+    static int digitosBinarios[8], indice = 0;
+
+    if (quociente < 1 || quociente > 128){
+        cout << "Número inválido";
+    }
+    else {
+        while (quociente > 0) {
+            resto = (quociente%2);
+            quociente = (quociente/2);
+            digitosBinarios[indice++] = resto;
+        }
+
+        for (int i = indice - 1; i >= 0; i--) {
+            cout << digitosBinarios[i];
+        }
+        cout << endl;
+        }
+    }
+
+int main(){
+    int num;
+    cout << "Digite um número entre 1 a 128: ";
+    cin >> num;
+
+    converte_em_binario(num);
+    return 0;
+}
