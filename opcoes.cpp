f3b509f0d533af61e146a104dedb626834a6eb69
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2024-12-07 19:26:46 -0300

    main

---
+++ opcoes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int num;
+    cout << "Digite um número entre 1 a 128: ";
+    cin >> num;
+
+    /*funcao(num)*/
+
+    return 0;
+}
